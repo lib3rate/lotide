e5ee0bed8e4480fdde4c606e77f2e9f41b45563b
Dmitry Petrov <e.liberate@gmail.com>
2020-05-27 01:25:13 +0000

    Added assertArraysEqual

---
+++ assertArraysEqual.js
@@ -0,0 +1,38 @@
+const eqArrays = function(firstArray, secondArray) {
+  let comparison = false;
+  if (firstArray.length !== secondArray.length) {
+    return false;
+  };
+  for (let i = 0; i < firstArray.length; i++) {
+    if (firstArray[i] === secondArray[i]) {
+      comparison = true;
+    } else {
+      return false;
+    }
+  }
+  return comparison;
+};
+
+const assertArraysEqual = function(firstArray, secondArray) {
+  let comparison = false;
+  if (firstArray.length !== secondArray.length) {
+    comparison = false;
+  };
+  for (let i = 0; i < firstArray.length; i++) {
+    if (firstArray[i] === secondArray[i]) {
+      comparison = true;
+    } else {
+      comparison = false;
+    }
+  }
+  if (comparison === true) {
+    console.log(`✅✅✅ Assertion Passed: ${firstArray} === ${secondArray}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${firstArray} !== ${secondArray}`);
+  };
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]) // => true
+assertArraysEqual([1, 2, 3], [3, 2, 1]) // => false
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]) // => true
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]) // => false
