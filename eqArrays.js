ba74d5b814c421938f758a9401cabe6caa7db634
Dmitry Petrov <e.liberate@gmail.com>
2020-05-27 00:44:36 +0000

    Added eqArrays

---
+++ eqArrays.js
@@ -0,0 +1,32 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(firstArray, secondArray) {
+  let comparison = false;
+  if (firstArray.length !== secondArray.length) {
+    return false;
+  };
+  for (let i = 0; i < firstArray.length; i++) {
+    if (firstArray[i] === secondArray[i]) {
+      comparison = true;
+    } else {
+      return false;
+    }
+  }
+  return comparison;
+};
+
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);
