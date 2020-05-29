404c40de3d4c4f18040b7f8a9c6e949a5c3d7660
Dmitry Petrov <e.liberate@gmail.com>
2020-05-29 00:49:18 +0000

    Added findKey

---
+++ findKey.js
@@ -0,0 +1,33 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(object, callback) {
+  for (let key in object) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+};
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma");
+
+assertEqual(findKey({
+  Toronto: { year: 1967 },
+  Ottawa:   { year: 1927 },
+  StLouis:      { year: 2019 },
+  Detroit:   { year: 2008 },
+  Chicago:       { year: 2015 },
+  Montreal:  { year: 1993 }
+}, x => x.year === 1967), "Toronto");
